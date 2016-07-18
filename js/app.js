914ac14ecb5f0476c446a3b590837d05c29e2dff
Andrew Young <prince.of.utah@gmail.com>
2016-07-18 16:44:54 -0400

    updated the route and the new function

--- js/app.js
+++ js/app.js
@@ -1,28 +1,28 @@
 "use strict";
 
 (function(){
-angular
+  angular
   .module("datingApp", [
     "ui.router",
     "profiles"
   ])
   .config(["$stateProvider",
-    RouterFunction
-  ])
+  RouterFunction
+])
 
 function RouterFunction($stateProvider){
   $stateProvider
-    .state("ProfileShow", {
-      url: "/profiles/:id",
-      templateUrl: "js/profiles/show.html",
-      controller: "ProfileShowController",
-      controllerAs: "ProfileShowViewModel"
-    })
-    .state("profileNew", {
-      url: "/profiles/new",
-      templateUrl: "js/profiles/new.html",
-      controller: "ProfileNewController",
-      controllerAs: "ProfileNewViewModel"
-  };
-}
+  .state("ProfileShow", {
+    url: "/profiles/:id",
+    templateUrl: "js/profiles/show.html",
+    controller: "ProfileShowController",
+    controllerAs: "ProfileShowViewModel"
+  })
+  .state("profileNew", {
+    url: "/profiles/new",
+    templateUrl: "js/profiles/new.html",
+    controller: "ProfileNewController",
+    controllerAs: "ProfileNewViewModel"
+  })
+};
 }());
