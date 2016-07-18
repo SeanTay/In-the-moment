8f07729f27d72dbd74812ed1a1b119fb3ac2199a
Sean Tay <seantay@Seans-MacBook-Pro-2.local>
2016-07-18 19:55:39 -0400

    working stories

--- js/profiles/show.controller.js
+++ js/profiles/show.controller.js
@@ -1,23 +1,31 @@
 "use strict";
 
 (function(){
-angular
+  angular
   .module("profiles")
   .controller("ProfileShowController", [
     "ProfileFactory",
+    "StoryFactory",
     "$stateParams",
+    "$state",
     ProfileShowFunction
   ])
 
-function ProfileShowFunction(ProfileFactory, $stateParams){
-  this.profile = ProfileFactory.get({id: $stateParams.id});
-  this.story = ProfileFactory.get({id: $stateParams.id});
-}
+  function ProfileShowFunction(ProfileFactory, StoryFactory, $stateParams, $state){
+    console.log("profile function");
+    this.profile = ProfileFactory.get({id: $stateParams.id });
 
-function StoriesShowFunction(){
-  console.log("stories")
-}
+    this.stories = StoryFactory.query({id: $stateParams.id});
 
+    this.story = new StoryFactory();
+
+    this.create = function(){
+      this.story.$save({id: $stateParams.id}).then(function(){
+        console.log('saving')
+        $state.go("ProfileShow", {}, {reload:true});
+      })
+    }
+  }
 
 
 }())
