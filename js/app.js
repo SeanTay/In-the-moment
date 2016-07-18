d58552bf5f01211eb2b1e175e423eaf01755769e
Sean Tay <seantay@Seans-MacBook-Pro-2.local>
2016-07-18 15:07:02 -0400

    working show page

--- js/app.js
+++ js/app.js
@@ -7,17 +7,13 @@ angular
     "profiles"
   ])
   .config(["$stateProvider",
-<<<<<<< HEAD
     RouterFunction
   ])
-=======
-  Router])
->>>>>>> 99d7a37d504a9220e963c8c6b2a9ad6f7adc285e
 
 function RouterFunction($stateProvider){
   $stateProvider
-    .state("ShowController", {
-      url: "/profile/:id",
+    .state("ProfileShow", {
+      url: "/profiles/:id",
       templateUrl: "js/profiles/show.html",
       controller: "ProfileShowController",
       controllerAs: "ProfileShowViewModel"
--- js/profiles/profile.factory.js
+++ js/profiles/profile.factory.js
@@ -3,7 +3,7 @@
 (function(){
 angular
   .module("profiles")
-  .factory("ProfileFactory", ["$resource", ProfileFactoryFunction ]);
+  .factory("ProfileFactory", ["$resource", ProfileFactoryFunction]);
 
 function ProfileFactoryFunction($resource){
   return $resource("http://localhost:3000/profiles/:id", {}, {
