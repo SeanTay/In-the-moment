147483f781418f0a66dc6b7b7b58ad4909ee93d8
Sean Tay <seantay@Seans-MacBook-Pro-2.local>
2016-07-19 17:57:05 -0400

    fixed bugs between routes

--- js/profiles/edit.controller.js
+++ js/profiles/edit.controller.js
@@ -11,15 +11,17 @@
     function ProfileEditControllerFunction(ProfileFactory, $stateParams, $state){
       this.profile = ProfileFactory.get({id: $stateParams.id})
 
+
       this.update = function(){
         this.profile.$update({id: $stateParams.id}).then(function(){
-          $state.go("ProfileShow",{},{reload:true})
+          $state.go("ProfileShow",{id: $stateParams.id},{reload:true})
         })
       }
 
       this.destroy = function(){
         this.profile.$delete({id: $stateParams.id}).then(function(){
-          $state.go("ProfileShow",{},{reload:true})
+          console.log("delete function working")
+          $state.go("WelcomePage",{},{reload:true})
         })
 
       }
