436117a44a1839664e7355fe2273c1608c4649fe
Sean Tay <seantay@Seans-MacBook-Pro-2.local>
2016-07-21 11:06:09 -0400

    changed factory address

--- js/profiles/story.factory.js
+++ js/profiles/story.factory.js
@@ -6,7 +6,7 @@
   .factory("StoryFactory", ["$resource", StoryFactoryFunction]);
 
   function StoryFactoryFunction($resource){
-    return $resource("http://localhost:3000/profiles/:profile_id/stories/:id", {}, {
+    return $resource("https://group7-datingapp.herokuapp.com/profiles/:profile_id/stories/:id", {}, {
       update: { method: "PUT" }
     });
   }
